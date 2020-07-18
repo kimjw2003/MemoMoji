ba2b1f652c2e541d5c7fe4ac7dcb2d8b102ca21f
kimjw2003 <kimjw111003@gmail.com>
2020-07-18 17:38:42 +0900

    [ADD] Dialog
    Dialog when Delete a memo

--- app/src/main/java/com/example/kimmemo/DetailActivity.kt
+++ app/src/main/java/com/example/kimmemo/DetailActivity.kt
@@ -8,35 +8,38 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
 import android.util.Log
+import android.view.LayoutInflater
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_detail.*
 import java.util.Calendar.getInstance
 
 class DetailActivity : AppCompatActivity() {
+    var memoDb : MemoDb? = null
+    var contents : String? = null
+    var title : String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
-        var memoDb : MemoDb? = null
+
         memoDb = MemoDb.getInstance(this)
 
-        val contents = intent.getStringExtra("content")
-        val title = intent.getStringExtra("title")
+        contents = intent.getStringExtra("content")
+        title = intent.getStringExtra("title")
 
         watch_edit_content.setMovementMethod(ScrollingMovementMethod()) //텍스트뷰 내에서 스크롤가능하게 해줌
 
         watch_edit_title.text = title
         watch_edit_content.text = contents
 
-        val delRunnable = Runnable { // delRunnable 쓰레드 제작
-            memoDb?.MemoDao()?.delete(title+"", contents+"") //OnClickListener에서 delRunnable쓰레드로 이동
-        }
+
 
         Del_button.setOnClickListener{//Delete버튼 클릭시
-            Thread(delRunnable).start() // delRunnable쓰레드 시작
-            var intent = Intent(this, MainActivity::class.java) //삭제 후 메인으로 이동
-            startActivity(intent)
-            finish()
+
+            showDialog()
         }
         copy_Button.setOnClickListener{
             clipBoard()
@@ -49,5 +52,25 @@ class DetailActivity : AppCompatActivity() {
         clipboardManager!!.setPrimaryClip(clipData)
 
         Toast.makeText(this, "클립보드에 복사되었습니다", Toast.LENGTH_SHORT).show()
+    }
+    private fun showDialog() {
+
+
+        val delRunnable = Runnable { // delRunnable 쓰레드 제작
+            memoDb?.MemoDao()?.delete(title+"", contents+"") //OnClickListener에서 delRunnable쓰레드로 이동
+        }
+
+        val alertDialog = AlertDialog.Builder(this)
+            .setTitle("정말로 삭제하시겠습니까?")
+            .setPositiveButton("삭제"){dialog, which->
+                Thread(delRunnable).start() // delRunnable쓰레드 시작
+                var intent = Intent(this, MainActivity::class.java) //삭제 후 메인으로 이동
+                startActivity(intent)
+                finish()
+            }
+            .setNeutralButton("취소", null)
+            .create()
+
+        alertDialog.show()
     }
 }
