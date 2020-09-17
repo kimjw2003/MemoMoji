386df2d216834f249f67691638b6b0cfb86febf8
kimjw2003 <kimjw111003@gmail.com>
2020-09-17 12:02:57 +0900

    commit

--- app/src/main/java/com/example/memoMoji/memoView/DetailActivity.kt
+++ app/src/main/java/com/example/memoMoji/memoView/DetailActivity.kt
@@ -17,12 +17,12 @@ import java.time.LocalDate
 class DetailActivity : AppCompatActivity() {
 
     //전역변수로 선언
-    private var memoDb : MemoDb? = null
-    private var contents : String? = null
-    var title : String? = null
+    private var memoDb: MemoDb? = null
+    private var contents: String? = null
+    var title: String? = null
     val time: LocalDate = LocalDate.now()
 
-    override fun onCreate(savedInstanceState: Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
@@ -34,18 +34,18 @@ class DetailActivity : AppCompatActivity() {
         watch_edit_title.text = title
         watch_edit_content.text = contents
 
-        delBtn.setOnClickListener{//Delete버튼 클릭시
+        delBtn.setOnClickListener {//Delete버튼 클릭시
             showDialog() //showDialog함수 실행
         }
-        copy_Button.setOnClickListener{//copy버튼 클릭시
+        copy_Button.setOnClickListener {//copy버튼 클릭시
             clipBoard()//clipBoard함수실행
         }
     }
 
     /*클립보드에 복사하는 함수*/
-    private fun clipBoard(){
+    private fun clipBoard() {
 
-        val clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE)as ClipboardManager
+        val clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
         val clipData = ClipData.newPlainText("label", watch_edit_content.text.toString())
         clipboardManager!!.setPrimaryClip(clipData)
 
@@ -56,13 +56,14 @@ class DetailActivity : AppCompatActivity() {
     private fun showDialog() {
 
         val delRunnable = Runnable { // delRunnable 쓰레드 제작
-            memoDb?.MemoDao()?.delete(title+"", contents+"") //OnClickListener에서 delRunnable쓰레드로 이동
+            memoDb?.MemoDao()
+                ?.delete(title + "", contents + "") //OnClickListener에서 delRunnable쓰레드로 이동
         }
 
         val alertDialog = AlertDialog.Builder(this)
             .setTitle("삭제")
-            .setMessage("정말 삭제하시겠습니까? \n"+"한번 삭제하면 복구 할 수 없습니다")
-            .setPositiveButton("삭제"){dialog, which-> //다이얼로그창에서 삭제버튼을 눌렀을 시 행동
+            .setMessage("정말 삭제하시겠습니까? \n" + "한번 삭제하면 복구 할 수 없습니다")
+            .setPositiveButton("삭제") { dialog, which -> //다이얼로그창에서 삭제버튼을 눌렀을 시 행동
                 Thread(delRunnable).start() // delRunnable쓰레드 시작
                 var intent = Intent(this, MainActivity::class.java) //삭제 후 메인으로 이동
                 startActivity(intent)
